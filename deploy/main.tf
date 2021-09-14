b68188ee9a81d0d9142db2ed37e528c1081b007d
Jason Ng <1995jasonng@gmail.com>
2021-09-14 16:27:51 +0930

    Added docker compose config for running terraform locally

---
+++ deploy/main.tf
@@ -0,0 +1,15 @@
+terraform {
+    backend "s3" {
+        bucket = "cont7-web-app-api-devops-tfstate"
+        key = "cont7-web-app-api.tfstate"
+        region = "us-east-1"
+        encrypt = true
+        dynamodb_table = "recipe-app-api-devops-tf-state-lock"
+    }
+}
+
+// define the service provider, since update of service provider may break the code
+provider "aws" {
+    region = "us-east-1"
+    version = "~> 2.54.0"
+}
