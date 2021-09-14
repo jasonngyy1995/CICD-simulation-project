b5e2333bc59c9fd58c7a81cb6e3b02d8b6dab3cc
Jason Ng <1995jasonng@gmail.com>
2021-09-14 18:23:01 +0930

    added terraform for creating bastion instance

---
+++ deploy/bastion.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name = "name"
+    # * -> retrieve the latest version
+    # most_recent = true
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "bastion" {
+  # match data field above
+  # resource in terraform, 'data' is for retrieving information from AWS
+  # create aws instance in our aws account
+  ami = data.aws_ami.amazon_linux.id
+  # Type -> how much resources will be assigned to that machine
+  instance_type = "t2.micro"
+}
+
+# In Terraform documentation, 'Data Source' is only for getting information
+# 'Resource' is for creating resources
