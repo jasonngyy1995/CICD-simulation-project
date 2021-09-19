e6a50be7749cb6fddb68070fbe618074a3806ea0
On Ki Ng <onki.ng@student.adelaide.edu.au>
2021-09-20 04:16:04 +0930

    Added bastion user data script

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -13,7 +13,8 @@ resource "aws_instance" "bastion" {
   # match data field above
   # resource in terraform, 'data' is for retrieving information from AWS
   # create aws instance in our aws account
-  ami = data.aws_ami.amazon_linux.id
+  ami       = data.aws_ami.amazon_linux.id
+  user_data = file("./templates/bastion/user-data.sh")
   # Type -> how much resources will be assigned to that machine
   instance_type = "t2.micro"
   # include base tag and the custom name tag of our resource
