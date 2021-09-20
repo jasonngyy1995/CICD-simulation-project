431da9e84eac7ba0c5ab27e14d6a052e5a669bf5
Portia Ng <portiang20@gmail.com>
2021-09-20 21:21:50 +0000

    Update app/app/settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+from pathlib import Path
 from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
